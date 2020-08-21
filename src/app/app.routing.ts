710083406d3d551bdb751d6eb98f4f3ecf3a9efa
Arturo Camacho <arturo.camacho.1dv@gmail.com>
2020-08-20 19:49:34 -0500

    Build del proyecto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,7 +11,7 @@ const appRoutes: Routes = [
     { path: '', component: LayoutComponent, children: [
         { path: '', redirectTo: '/home', pathMatch: 'full' },
         { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-        { path: 'products',  loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+        { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
         { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
         { path: 'order', loadChildren: () => import('./order/order.module').then(m => m.OrderModule) },
         { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
